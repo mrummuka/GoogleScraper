f6a4837604659fe8ac041883fc56a42c67367b59
mrummuka <mrummuka@gmail.com>
2016-05-18 21:32:13 +0300

    Add command-line parsing --remote-uri <uri> for remote browser method

--- GoogleScraper/commandline.py
+++ GoogleScraper/commandline.py
@@ -29,8 +29,13 @@ def get_command_line(only_print_help=False):
                              'second if you have the necessary number of proxies available. ',
                         choices=('http', 'selenium', 'http-async'))
 
-    parser.add_argument('--sel-browser', choices=['firefox', 'chrome', 'phantomjs'], default='phantomjs',
+    parser.add_argument('--sel-browser', choices=['firefox', 'chrome', 'phantomjs', 'remotechrome'], default='phantomjs',
                         help='The browser frontend for selenium scraping mode. Does only make sense if --scrape-method is set to "selenium"')
+
+    parser.add_argument('-r', '--remote-uri', type=str, action='store', default='http://localhost:4444/wd/hub',
+                        help='URI for connecting with remote selenium/chrome installation.' 
+			     'Only necessary if --sel-browser=remotechrome has been chosen.')
+
 
     keyword_group = parser.add_mutually_exclusive_group()
 
